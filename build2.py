b3f803c7dfa40d1d88d2d755c5110f1bf0635301
newtom <newtom@pobox.com>
2022-06-06 13:04:22 -0700

    timestamp

--- build2.py
+++ build2.py
@@ -2,6 +2,7 @@ import yaml
 from flatten_dict import flatten
 import sys
 import logging
+from datetime import datetime
 
 def Add(a, b):
     return a+b
@@ -11,5 +12,6 @@ if __name__ == '__main__':
         res = yaml.safe_load(f)
         kv = flatten(res, reducer='dot')
         print(kv)
-        logging.warning(f"2 + 3 = {Add(2,3)}")
+        logging.warning(f' [{sys.argv[0]}@{datetime.now().isoformat()}] ' +
+            f"2+3 = {Add(2,3)}")
 
--- test2.py
+++ test2.py
@@ -1,10 +1,13 @@
-from main1 import Add
+from build2 import Add
+import sys
 import logging
+from datetime import datetime
 
 def TestAdd():
     assert Add(2,3) == 5
-    print("Test passed")
+    print("assert test2")
 
 if __name__ == '__main__':
     TestAdd()
-    logging.warning("end of test1")
+    logging.warning(f' [{sys.argv[0]}@{datetime.now().isoformat()}] ' +
+            '"passed"')
